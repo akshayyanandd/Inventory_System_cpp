32ff2a8140f771765596c6a8ed2304c011c8f8e2
AKSHAY ANAND <akshayyanandd@gmail.com>
2024-07-25 21:52:32 +0530

    Create main.cpp
    The main.cpp is save on github

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdlib>
+#include<windows.h>
+
+#include "Snake.h"
+// MACROS
+
+
+#define WIDTH 50
+#define HEIGHT 25
+
+using namespace std;
+
+int x = 10, y = 10;
+
+void board()
+{
+    for (int i = 0; i < HEIGHT; i++)
+    {
+        cout << "\t\t#";
+        for (int j = 0; j < WIDTH; j++)
+        {
+            if (i == 0 || i == HEIGHT - 1)
+                cout << "#";
+
+            else if (i == y && j == x)
+                cout << "0";
+            else
+                cout << " ";
+        }
+        cout << "#\n";
+    }
+}
+int main()
+{
+    while (true)
+    {
+        board();
+        x++;
+        // THIS IS FUNCTION OF WINDOWS.H AND IS USED TO REMOVE FLICKERING COMING FROM THE CLEARINING SCREEN
+         //system("clr");
+       SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE) , { 0 , 0 });
+    }
+    return 0;
+}
