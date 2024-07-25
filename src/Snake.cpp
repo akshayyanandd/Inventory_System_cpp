36adfd1951cfc0fdfe47fb7fd3157e4b634b154c
AKSHAY ANAND <akshayyanandd@gmail.com>
2024-07-25 21:54:58 +0530

    feat: the snake header file and snake.cpp file added

---
+++ src/Snake.cpp
@@ -0,0 +1,24 @@
+#ifndef SNAKE_H
+#define SNAKE_H
+#include <windows.h>
+
+
+class Snake
+{
+private:
+// this COORD is in windows.h
+    COORD pos;
+    int len;
+    int vel;
+    char direction;
+public:
+    
+    Snake(COORD pos,int vel);
+    
+    void change_dir(char dir);
+    void move_snake();
+    COORD get_pos();
+    
+};
+
+#endif
